e37c1aa2aea092353511af7c5668f3f5ba0346b3
chasinghope <42870306+chasinghope@users.noreply.github.com>
2023-12-26 10:04:01 +0800

    Update DataModelBase.cs

--- Assets/Congroo/DataModel/DataModelBase.cs
+++ Assets/Congroo/DataModel/DataModelBase.cs
@@ -1,20 +1,24 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace Congroo.Core
 {
     public class DataModelBase
     {
         protected List<EventWrapper> mEventWrappers;
+        protected CancellationTokenSource CancelTokenSource;
 
         public virtual void Initialize()
         {
+            CancelTokenSource = new CancellationTokenSource();
             EventCenter.Instance.BindEventWrappers(mEventWrappers);
         }
 
 
-        public virtual void Dispose()
+        public virtual void Release()
         {
+            CancelTokenSource.Cancel();
             EventCenter.Instance.UnbindEventWrappers(mEventWrappers);
         }
 
