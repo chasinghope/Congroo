e4e87d3fd5cf1f9c97b035d5eb30721ddc506f29
chasinghope <42870306+chasinghope@users.noreply.github.com>
2023-12-26 10:02:16 +0800

    Update UIManager.cs

--- Assets/Congroo/UI/Base/UIManager.cs
+++ Assets/Congroo/UI/Base/UIManager.cs
@@ -1,6 +1,8 @@
 using Congroo.Core;
+using Cysharp.Threading.Tasks;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
@@ -25,18 +27,28 @@ public class UIManager : SingletonMono<UIManager>
 
     private Dictionary<string, UIPageBase> Page_LoadedDict = new Dictionary<string, UIPageBase>();
     private Dictionary<string, UIPopupBase> Popup_LoadedDict = new Dictionary<string, UIPopupBase>();
+
+    private Stack<string> mPageStack = new Stack<string>();
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+        GameObject.DontDestroyOnLoad(gameObject);
+    }
+
     private async void Start()
     {
         await Initialize();
     }
-    public async Task Initialize()
+    public async UniTask Initialize()
     {
         await Preload();
-        await this.OpenPageUI(UIViewName.MainMenu);
+        await this.OpenPageUI(UIViewName.MainView);
     }
 
 
-    public async Task Preload()
+    public async UniTask Preload()
     {
         foreach (var item in PreLoadList)
         {
@@ -69,7 +81,7 @@ public class UIManager : SingletonMono<UIManager>
         }
     }
 
-    public async Task<T> SpawnUI<T>(string rUName) where T : UIBase
+    public async UniTask<T> SpawnUI<T>(string rUName) where T : UIBase
     {
         string path = GetAddressablePath(rUName);
         CLog.L(LType.UI, $"SpawnUI<T> {path}");
@@ -89,15 +101,25 @@ public class UIManager : SingletonMono<UIManager>
         go.transform.localScale = Vector3.one;
         T uiBase = go.GetComponent<UIBase>() as T;
         uiBase.UName = prefab.name;
+        uiBase.Asset = op;
+        uiBase.Init();
         return uiBase;
     }
 
 
-    public async Task OpenPageUI(string rUName)
+    public async UniTask OpenPageUI(string rUName)
     {
+        // �رյ�ǰ����
+        string prePage = string.Empty;
+        if(mPageStack.Count > 0)
+        {
+            prePage = mPageStack.Peek();
+        }
+
         if (this.Page_LoadedDict.TryGetValue(rUName, out UIPageBase uiBase))
         {
             uiBase.Open();
+            mPageStack.Push(rUName);
         }
         else
         {
@@ -107,10 +129,51 @@ public class UIManager : SingletonMono<UIManager>
             uiPageBase.transform.SetAsLastSibling();
             Page_LoadedDict.Add(uiPageBase.UName, uiPageBase);
             uiPageBase.Open();
+            mPageStack.Push(rUName);
+        }
+
+        if(!string.IsNullOrEmpty(prePage))
+        {
+            ClosePageUI(prePage);
         }
     }
 
-    public async Task OpenPopupUI(string rUName)
+    public async UniTask BackPage()
+    {
+        if(mPageStack.Count > 1)
+        {
+            // �رյ�ǰ����
+            string prePage = mPageStack.Pop();
+            string curPage = mPageStack.Pop();
+
+            if (this.Page_LoadedDict.TryGetValue(curPage, out UIPageBase uiBase))
+            {
+                uiBase.Open();
+                mPageStack.Push(curPage);
+            }
+            else
+            {
+                UIPageBase uiPageBase = await SpawnUI<UIPageBase>(curPage);
+
+                uiPageBase.transform.SetParent(PageRoot, false);
+                uiPageBase.transform.SetAsLastSibling();
+                Page_LoadedDict.Add(uiPageBase.UName, uiPageBase);
+                uiPageBase.Open();
+                mPageStack.Push(curPage);
+            }
+
+            if (!string.IsNullOrEmpty(prePage))
+            {
+                ClosePageUI(prePage);
+            }
+        }
+        else
+        {
+            CLog.L(LType.UI, $"���һ��������,�޷�����");
+        }
+    }
+
+    public async UniTask OpenPopupUI(string rUName)
     {
         if (this.Popup_LoadedDict.TryGetValue(rUName, out UIPopupBase uiBase))
         {
@@ -128,7 +191,7 @@ public class UIManager : SingletonMono<UIManager>
     }
 
 
-    public void ClosePageUI(string rUName)
+    private void ClosePageUI(string rUName)
     {
         if (this.Page_LoadedDict.TryGetValue(rUName, out UIPageBase uiBase))
         {
@@ -137,7 +200,7 @@ public class UIManager : SingletonMono<UIManager>
             {
                 GameObject.Destroy(uiBase.gameObject);
                 this.Page_LoadedDict.Remove(rUName);
-                ResMgr.Instance.ReleaseAsset(uiBase.gameObject);
+                ResMgr.Instance.Relase(uiBase.Asset);
             }
         }
     }
@@ -152,7 +215,7 @@ public class UIManager : SingletonMono<UIManager>
             {
                 GameObject.Destroy(uiBase.gameObject);
                 this.Popup_LoadedDict.Remove(rUName);
-                ResMgr.Instance.ReleaseAsset(uiBase.gameObject);
+                ResMgr.Instance.Relase(uiBase.Asset);
             }
         }
     }
@@ -180,7 +243,7 @@ public class UIManager : SingletonMono<UIManager>
 
     private string GetAddressablePath(string rUName)
     {
-        return $"Assets/GameAssets/Prefabs/UI/{rUName}.prefab";
+        return $"Prefabs/UI/{rUName}.prefab";
     }
 
 }
